1b8db1bb8a14eb165f7d4ba43a4871b33c799350
Richard Guedes <45791401+richardg7@users.noreply.github.com>
2025-04-19 09:37:07 -0300

    Update version.php
    Atualização da versão 1.0 para 1.1.

--- version.php
+++ version.php
@@ -5,4 +5,4 @@ $plugin->component = 'local_sso_login';
 $plugin->version   = 2025041301;
 $plugin->requires  = 2020061500;
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.0';
+$plugin->release   = '1.1';
